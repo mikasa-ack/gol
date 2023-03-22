046454939f8bd1c6c6ebdda3d45cb5ed3b02aae1
Abdel @ StarkWare <abdelhamid.bakhta@gmail.com>
2023-03-22 18:53:39 +0100

    wip

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,8 @@
 !/bin/bash
+
+cache_dir=.cache
+mkdir -p $cache_dir
+
 grid_file=$1
 echo "Building contract..."
 cargo contract build
@@ -10,3 +14,5 @@ echo "Instantiating contract..."
 grid=$(cat $grid_file)
 contract_address=$(cargo contract instantiate --suri //Alice --args "$grid" --execute --output-json --skip-confirm | jq .contract)
 echo "Contract address: $contract_address"
+# Write contract address to cache
+echo $contract_address > $cache_dir/contract_address
---
+++ scripts/unstop.sh
@@ -0,0 +1,9 @@
+!/bin/bash
+
+cache_dir=.cache
+contract_address_file=$cache_dir/contract_address
+
+# Read contract address from cache
+contract_address=$(cat $contract_address_file)
+
+cargo contract call --contract $contract_address --suri //Alice --message set_manual_stop --args false --execute --skip-confirm
